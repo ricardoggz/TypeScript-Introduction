2f8e187002557854a4c1ed28bd83b98cbb4953d0
ricardoggz <ricardo_ggzicm@outlook.com>
2022-06-02 16:06:03 -0500

    Leccion de Arrays en TypeScript agregada

---
+++ bases-TS/DataTypes/arrays.js
@@ -0,0 +1,5 @@
+"use strict";
+/*Declarar un array en TypeScript:*/
+var firstExampleArray = ["Ricardo", "José", "Ernesto"]; //le indico el tipo de dato que quiero en mi arreglo (number[])
+/*Declarar un array de diversos tipos de dato en TypeScript:*/
+var exampleArray = [1, "2", 3, "4"];
---
+++ bases-TS/DataTypes/arrays.ts
@@ -0,0 +1,5 @@
+/*Declarar un array en TypeScript:*/
+const firstExampleArray : string[] = ["Ricardo", "José", "Ernesto"]; //le indico el tipo de dato que quiero en mi arreglo (number[])
+
+/*Declarar un array de diversos tipos de dato en TypeScript:*/
+const exampleArray: (number | string)[] = [1, "2", 3, "4"];
