bbb2c8e97ae590f46208b1a7d9ea8b1d341da573
ricardoggz <ricardo_ggzicm@outlook.com>
2022-06-03 10:10:58 -0500

    leccion de tuplas agregada

---
+++ bases-TS/DataTypes/tuples.js
@@ -0,0 +1,5 @@
+"use strict";
+/*Declarar una tupla en TypeScript:*/
+//similares a un arreglo, pero con la cualidad de poder 
+//controlar los tipos de datos y la longitud del arrelo
+var exampleTuple = ["ricardo", 21, false];
---
+++ bases-TS/DataTypes/tuples.ts
@@ -0,0 +1,6 @@
+/*Declarar una tupla en TypeScript:*/
+
+//similares a un arreglo, pero con la cualidad de poder 
+//controlar los tipos de datos y la longitud del arrelo
+
+const exampleTuple : [string, number, boolean] = ["ricardo", 21, false];
