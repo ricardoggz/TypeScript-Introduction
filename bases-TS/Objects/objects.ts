b9414ac4cc976ddf44573af226a472d6b6c5649b
ricardoggz <ricardo_ggzicm@outlook.com>
2022-06-11 15:23:41 -0500

    leccion de objetos literales agregada

---
+++ bases-TS/Objects/objects.ts
@@ -0,0 +1,14 @@
+/*Objetos literales en TypeScript*/
+
+let task = {
+  //TypeScript asignará el valor del objeto como un tipo de dato.
+  description: "Complete the TypeScript course",
+  reference: "Typescript for beginners",
+  completed: false,
+};
+
+task = {
+  description: "Complete the JavaScript Course",
+  reference: "JavaScript Course",
+  completed: true,
+};
