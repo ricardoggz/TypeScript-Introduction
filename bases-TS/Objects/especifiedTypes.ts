92ae147afbff7f29e913c2424aab8f5658b8be74
ricardoggz <ricardo_ggzicm@outlook.com>
2022-06-13 09:44:49 -0500

    Leccion de objetos con tipado especifico argegada

---
+++ bases-TS/Objects/especifiedTypes.ts
@@ -0,0 +1,9 @@
+/*Declaración de objetos con tipado de datos*/
+
+(() => {
+  let user: { name: string; nickname: string, } = {
+    name: "Juan Pérez",
+    nickname: "@juan_perez",
+  };
+  console.log(user);
+})();
